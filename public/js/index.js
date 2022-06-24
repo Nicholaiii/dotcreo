4c301710825ed93cd630a315755956390604c09e
Kinger <sam@exagmitedigital.com>
2022-06-24 08:53:26 +0100

    24/06/2022

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,25 @@
-const upperNavBar = document.querySelector('.upper-navbar')
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
