daf77298573f445a8c11f6c21ac128eccdd8d0e2
samkingtwentytwo <samkingtwentytwo@gmail.com>
2022-11-16 07:52:12 +0000

    16/11/2022

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ app.get('/blog', (req, res) => {
     res.sendFile(__dirname + '/public/blog.html')
 })
 
+app.get('/the-modern-attention-span', (req, res) => {
+    res.sendFile(__dirname + '/public/the-modern-attention-span_blog.html')
+})
+
+app.get('/website-seo-checklist', (req, res) => {
+    res.sendFile(__dirname + '/public/website-seo-checklist_blog.html')
+})
+
+app.get('/what-makes-a-great-website', (req, res) => {
+    res.sendFile(__dirname + '/public/what-makes-a-great-website_blog.html')
+})
+
 
 app.listen(2022, () => {
     console.log(`This app is listening on port 2022`)
